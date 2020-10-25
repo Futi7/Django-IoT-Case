873d964f8c8643f30397049ff12b326810222afc
futi7 <fuatseven1997@hotmail.com>
2020-10-25 18:47:32 +0300

    Added allowed hosts

--- djangoCase/settings.py
+++ djangoCase/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+s4les_lb#3f6zil2%&!2q@8e4d!fyg-=x_9kfovgrpd@#6nk$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [django-rest-case.herokuapp.com]
 
 
 # Application definition
